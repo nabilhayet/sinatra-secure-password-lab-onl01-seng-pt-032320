bd5908b956396fb5fc2b4c19c7ebc86d50568e28
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-27 03:01:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   else
   user = User.new(:username => params[:username], :password => params[:password])
 
-  if user 
+  if user.save 
     redirect "/login"
   else
     redirect "/failure"
