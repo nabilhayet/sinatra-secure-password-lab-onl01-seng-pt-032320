660b429a66141b9775738158e4350b82ca7b1575
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-27 03:03:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ end
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/success"
+      redirect "/account"
     else
       redirect "/failure"
     end
